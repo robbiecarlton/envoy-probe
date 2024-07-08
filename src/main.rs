59e2052ea3f3f44b91a3d9a70ec1c2cde48665af
Robbie Carlton <robbie.carlton@gmail.com>
2024-07-08 13:02:35 -0600

    add ports

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,15 @@ use anyhow::Result;
 
 // the installed_app_id of the single hosted happ in holo dev server
 pub const HDS_HOSTED_HAPP_ID: &str = "uhCkkCQHxC8aG3v3qwD_5Velo1IHE1RdxEr9-tuNSK15u63m1LPOo";
+// these are the default app ports for HDS. Should only update them if you've changed the ports when running HDS
+pub const ADMIN_PORT: u16 = 4444;
+pub const APP_PORT: u16 = 4565;
+
 
 #[tokio::main]
 async fn main() {
     let admin_ws = 
-        AdminWebsocket::connect((std::net::Ipv4Addr::LOCALHOST, 4444)).await
+        AdminWebsocket::connect((std::net::Ipv4Addr::LOCALHOST, ADMIN_PORT)).await
         .unwrap_or_else(|e| panic!("Failed to connect admin_ws {:?}", e));
     
     let app_interfaces = admin_ws.list_app_interfaces().await.expect("failed on list_app_interfaces");
@@ -22,7 +26,7 @@ async fn main() {
 }
 
 
-// used if you need to make an app ws call
+// used if you need to make an app ws call.
 async fn get_authenticated_app_ws(
     admin_ws: &AdminWebsocket,
     app_id: String,
